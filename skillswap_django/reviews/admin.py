081ec91fabf8ae814cedd9b4c9e0939b20acadfb
HajarElMannani <laso125@hotmail.com>
2025-06-29 14:59:30 +0100

    Delete imports

--- skillswap_django/reviews/admin.py
+++ skillswap_django/reviews/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import Review
-from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied
-from django.db import models
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
