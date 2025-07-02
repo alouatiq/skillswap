3f720766e094cc223d85ceb1e10aeda30500ac6c
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-07-02 10:19:56 +0200

    Update settings.py

--- skillswap_django/skillswap_backend/settings.py
+++ skillswap_django/skillswap_backend/settings.py
@@ -5,6 +5,7 @@ import dj_database_url
 from datetime import timedelta
 from celery.schedules import crontab
 from corsheaders.defaults import default_headers
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
